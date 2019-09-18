96800317605eb0d9d36283059d852c5f19d4548a
Student <example@example.com>
2019-09-18 14:56:08 -0700

    Configure the React-Redux <Provider>

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,15 @@ import { AppContainer } from 'react-hot-loader'
 import { HashRouter } from 'react-router-dom'
 import { createStore } from 'redux';
 import ticketListReducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <HashRouter>
+      <Provider store={store} >
         <Component/>
+        </Provider>
       </HashRouter>
     </AppContainer>,
     document.getElementById('react-app-root')
